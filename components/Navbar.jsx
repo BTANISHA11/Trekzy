014d30de7a0161bb7951bfa4f67faa63f9afacca
TANISHA BANSAL <tanishab130@gmail.com>
2023-09-16 00:11:20 +0530

    final

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,10 @@
 'use client';
+
 import { motion } from 'framer-motion';
+
 import styles from '../styles';
 import { navVariants } from '../utils/motion';
+
 const Navbar = () => (
   <motion.nav
     variants={navVariants}
@@ -10,17 +13,22 @@ const Navbar = () => (
     className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
-    {/* navbar */}
-    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      <img src="/search.svg" 
-      alt="search"
-      className="w-[24px] h-[24px] object-contain"
+    <div
+      className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+    >
+      <img
+        src="/search.svg"
+        alt="search"
+        className="w-[24px] h-[24px] object-contain"
       />
-      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
-METAVERSUS
+      <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
+        METAVERSUS
       </h2>
-      <img src="/menu.svg" alt="menu"
-      className="w-[24px] h-[24px] object-contain" />
+      <img
+        src="/menu.svg"
+        alt="menu"
+        className="w-[24px] h-[24px] object-contain"
+      />
     </div>
   </motion.nav>
 );
