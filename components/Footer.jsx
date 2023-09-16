d3ccee8a9e5d00b8694a821790f7a5ba60aa3485
TANISHA BANSAL <tanishab130@gmail.com>
2023-09-16 12:06:35 +0530

    update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,6 +5,7 @@ import { socials } from '../constants';
 
 import styles from '../styles';
 import { footerVariants } from '../utils/motion';
+// import { logo } from "../assets";
 
 const Footer = () => (
   <motion.footer
@@ -30,7 +31,48 @@ const Footer = () => (
           </span>
         </button>
       </div>
-
+{/* // extra one  */}
+<div className="flex justify-center items-start md:flex-row flex-col mb-8 w-full">
+      <div className="flex-1 flex flex-col justify-start mr-10">
+        <img
+          src="/assets/logo.51b9a1fd.svg"
+          alt="hoobank"
+          className="w-[266px] h-[72.14px] object-contain"
+        />
+        <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] mt-4 max-w-[312px]">
+          A new way to make payments easy, reliable, and secure.
+        </p>
+      </div>
+      <div className="flex-1.5 w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
+        <div className="flex flex-col ss:my-0 my-4 min-w-[150px]">
+          <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">Useful Links</h4>
+          <ul className="list-none mt-4">
+            <li className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer mb-4">Content</li>
+            <li className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer mb-4">How it Works</li>
+            <li className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer mb-4">Create</li>
+            <li className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer mb-4">Explore</li>
+            <li className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer mb-0">Terms &amp; Services</li>
+          </ul>
+        </div>
+        <div className="flex flex-col ss:my-0 my-4 min-w-[150px]">
+          <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">Community</h4>
+          <ul className="list-none mt-4">
+            <li className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer mb-4">Help Center</li>
+            <li className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer mb-4">Partners</li>
+            <li className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer mb-4">Suggestions</li>
+            <li className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer mb-4">Blog</li>
+            <li className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer mb-0">Newsletters</li>
+          </ul>
+        </div>
+        <div className="flex flex-col ss:my-0 my-4 min-w-[150px]">
+          <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">Partner</h4>
+          <ul className="list-none mt-4">
+            <li className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer mb-4">Our Partner</li>
+            <li className="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer mb-0">Become a Partner</li>
+          </ul>
+        </div>
+      </div>
+    </div>
       <div className="flex flex-col">
         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
@@ -59,3 +101,8 @@ const Footer = () => (
 );
 
 export default Footer;
+
+
+
+
+  
